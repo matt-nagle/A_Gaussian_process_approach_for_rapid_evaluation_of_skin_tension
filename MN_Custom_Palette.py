a3b337f4acd6f964435f72b27f94be011e41e486
mattnagle <matt.nagle@ucdconnect.ie>
2024-04-03 15:50:06 +0100

    initial commit

---
+++ MN_Custom_Palette.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+
+import numpy as np
+
+
+mn_colours = ["#5e81b5",
+             "#e19c24",
+             "#3ba545",
+             "#ff6b97",
+             "#2bbcce"]
+
+mn_dpi = 500
+
+mn_square_fig_size = (10,10)
+mn_rectangular_fig_size = (10,7)
+
+mn_title_fontsize = 28
+mn_axis_fontsize = 22
+mn_ticks_fontsize = 18
+
+mn_legend_fontsize = 18
